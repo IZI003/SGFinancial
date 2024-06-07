1b920ef6a6d05d3a5ebcf0336d901d255a9bf2ca
Ezequiel.Miranda <mirandaezquiel@gmail.com>
2024-06-07 18:02:59 -0300

    se eliminan archivos inecesarios

--- Operaciones/Modelos/EntradaCrearOperacion.cs
+++ Operaciones/Modelos/EntradaCrearOperacion.cs
@@ -4,5 +4,6 @@ public class EntradaCrearOperacion
 {
    public int idCuenta { get; set; }
     public int idTipoOperacion { get; set; }
+    public int idConcepto { get; set; }
     public decimal monto { get; set; }
 }
